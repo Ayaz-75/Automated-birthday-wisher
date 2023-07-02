699cdfe3a370308e1a49f6e9db9640c4cba1ed68
Ayaz Lakho <82840513+Ayaz-75@users.noreply.github.com>
2023-07-03 00:58:34 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-#
-#
-# import smtplib
-#
-# my_email = "hisherhim2022@gmail.com"
-# my_password = "fqhbljpavieykgoz"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=my_password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="lakho75@yahoo.com",
-#         msg="Hello!"
-#     )
-#
-#
-
-#
-#
-#
-# import datetime as dt
-#
-# date_of_birth = dt.datetime(year=2000, month=6, day=5, hour=4, minute=45, second=35)
-# print(f"My Date of birth is: {date_of_birth}")
-
-
 import datetime as dt
 import random
 import smtplib
 
-my_email = "hisherhim2022@gmail.com"
-my_password = "fqhbljpavieykgoz"
+my_email = "Your email"
+my_password = "Your pass"
 
 now = dt.datetime.now()
 weekday = now.weekday()
@@ -45,9 +18,37 @@ if weekday == 0:
         connection.login(user=my_email, password=my_password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="lakho75@yahoo.com",
+            to_addrs="abc@example.com",
             msg=random_quote
         )
 
 
 print("Sent successfully")
+
+
+
+# import smtplib
+
+# my_email = "Your email"
+# my_password = "Your pass"
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="receiving_@mail.com",
+#         msg="Hello!"
+#     )
+
+#
+
+#
+#
+#
+# import datetime as dt
+#
+# date_of_birth = dt.datetime(year=2000, month=6, day=5, hour=4, minute=45, second=35)
+# print(f"My Date of birth is: {date_of_birth}")
+
+
